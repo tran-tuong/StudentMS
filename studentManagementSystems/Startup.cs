8caec759da1d3b250ea565668f24b0d275bc69f8
tran-tuong <tranvantuong2k3@gmail.com>
2024-05-15 10:46:45 +0700

    create project

---
+++ studentManagementSystems/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(studentManagementSystems.Startup))]
+namespace studentManagementSystems
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
